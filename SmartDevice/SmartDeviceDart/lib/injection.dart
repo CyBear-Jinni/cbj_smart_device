55692c02d268768adb81ee3d090eecfac8b8889a
Guy Luz <guyluz11@gmail.com>
2020-02-22 21:48:16 +0200

    Made the

---
+++ SmartDevice/SmartDeviceDart/lib/injection.dart
@@ -0,0 +1,16 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'injection.iconfig.dart';
+
+final getIt = GetIt.instance;
+
+@injectableInit
+Future<void> configureInjection(String environment) async =>
+    await $initGetIt(getIt, environment: environment);
+
+abstract class Env {
+  static const test = 'test';
+  static const dev = 'dev';
+  static const prod = 'prod';
+}
