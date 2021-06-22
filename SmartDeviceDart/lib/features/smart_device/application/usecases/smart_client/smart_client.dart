19837c5e93846699032b01017b95daa0ccb0fbff
Guy Luz <guyluz11@gmail.com>
2021-06-22 14:14:36 +0300

    Add client test for smart device for talk to the hub.

---
+++ SmartDeviceDart/lib/features/smart_device/application/usecases/smart_client/smart_client.dart
@@ -0,0 +1,36 @@
+import 'package:grpc/grpc.dart';
+import 'package:smart_device_dart/features/smart_device/infrastructure/datasources/smart_server_d/protoc_as_dart/smart_connection.pbgrpc.dart';
+
+class SmartClient {
+  static ClientChannel? channel;
+  static SmartServerClient? stub;
+
+  ///  Turn smart device on
+  static Future<void> createStreamWithClients(String addressToHub) async {
+    channel = await createSmartServerClient(addressToHub);
+    stub = SmartServerClient(channel!);
+    ResponseStream<ClientStatusRequests> response;
+    Stream<RequestsAndStatusFromHub> streamClientStatusRequests =
+        Stream.value(RequestsAndStatusFromHub());
+    try {
+      response = stub!.registerHub(streamClientStatusRequests);
+      response.listen((value) {
+        print('Greeter client received: $value');
+      });
+// await channel!.shutdown();
+// return response.success.toString();
+    } catch (e) {
+      print('Caught error: $e');
+    }
+// await channel!.shutdown();
+// throw 'Error';
+  }
+
+  static Future<ClientChannel> createSmartServerClient(String deviceIp) async {
+    await channel?.shutdown();
+    return ClientChannel(deviceIp,
+        port: 50051,
+        options:
+            const ChannelOptions(credentials: ChannelCredentials.insecure()));
+  }
+}
