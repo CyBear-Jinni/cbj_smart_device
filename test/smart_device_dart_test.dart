7b8a02eb8a84570f52b04f2ae458b91ba5d56e7d
guyluz11 <guyluz11@gmail.com>
2022-10-06 14:43:47 +0300

    Hopefully fixed git action not passing

--- test/smart_device_dart_test.dart
+++
@@ -1,8 +0,0 @@
-//import 'package:cbj_smart_device/emptyConsoleApplicationDart.dart';
-//import 'package:test/test.dart';
-//
-void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-}
