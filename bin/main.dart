847c816d301fbaf6a642c78c8761d9a84b643016
guyluz11 <guyluz11@gmail.com>
2022-10-07 22:52:56 +0300

    Removed the goToSuspend test

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,6 @@
 import 'package:cbj_smart_device/application/usecases/core_u/smart_device_manager_u.dart';
 import 'package:cbj_smart_device/core/shared_variables.dart';
 import 'package:cbj_smart_device/infrastructure/datasources/core_d/manage_physical_components/device_pin_manager.dart';
-import 'package:cbj_smart_device/infrastructure/repositories/core_r/my_singleton_helper.dart';
 import 'package:cbj_smart_device/utils.dart';
 
 void main(List<String> arguments) async {
@@ -18,5 +17,5 @@ void main(List<String> arguments) async {
   await DevicePinListManager().setPhysicalDeviceType();
 
   SmartDeviceManagerU();
-  MySingletonHelper.goToSuspend();
+  // MySingletonHelper.goToSuspend();
 }
