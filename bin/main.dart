503cdc7dec7408cec4f7e63a33f1caa36bad3f85
Guy Luz <guyluz11@gmail.com>
2023-11-07 11:35:01 +0200

    Removed injectable dependency

--- bin/main.dart
+++ bin/main.dart
@@ -2,13 +2,10 @@ import 'package:cbj_integrations_controller/infrastructure/shared_variables.dart
 import 'package:cbj_integrations_controller/infrastructure/system_commands/system_commands_manager_d.dart';
 import 'package:cbj_smart_device/application/usecases/core_u/smart_device_manager_u.dart';
 import 'package:cbj_smart_device/infrastructure/datasources/core_d/manage_physical_components/device_pin_manager.dart';
-import 'package:cbj_smart_device/injection.dart';
 import 'package:cbj_smart_device/utils.dart';
 
 void main(List<String> arguments) async {
   logger.i('Smart device is activated');
-
-  await configureInjection(Env.prod);
 
   try {
     SystemCommandsManager();
