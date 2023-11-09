d230101ab1e5b12bda6f319fe6b31074ce10625b
Guy Luz <guyluz11@gmail.com>
2023-11-09 21:56:42 +0200

    Updated packages

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,19 @@
+import 'dart:io';
+
 import 'package:cbj_integrations_controller/infrastructure/shared_variables.dart';
 import 'package:cbj_integrations_controller/infrastructure/system_commands/system_commands_manager_d.dart';
 import 'package:cbj_smart_device/application/usecases/core_u/smart_device_manager_u.dart';
 import 'package:cbj_smart_device/infrastructure/datasources/core_d/manage_physical_components/device_pin_manager.dart';
 import 'package:cbj_smart_device/utils.dart';
+import 'package:network_tools/network_tools.dart';
 
 void main(List<String> arguments) async {
   logger.i('Smart device is activated');
 
   try {
+    configureNetworkTools('network_tools_db');
     SystemCommandsManager();
-    SharedVariables(arguments[0]);
+    SharedVariables(arguments.firstOrNull ?? Directory.current.path);
   } catch (error) {
     logger.w('Path/argument 1 is not specified\n$error');
   }
